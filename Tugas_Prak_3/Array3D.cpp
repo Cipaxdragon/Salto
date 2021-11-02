f3fae6a0784592cec9eb34de20d21719cc5e5584
Asep <88421251+Cipaxdragon@users.noreply.github.com>
2021-11-03 00:00:34 +0800

    021

--- Tugas_Prak_3/Array3D.cpp
+++ Tugas_Prak_3/Array3D.cpp
@@ -7,17 +7,20 @@ int main(){
         {{1,2},{3,4}},
         {{5,6},{7,8}}
     };
-    cout << angka[0][0][0] << endl;//1
-    cout << angka[0][0][1] << endl;//2
     
     cout << angka[0][0][0] << endl;//1
     cout << angka[0][0][1] << endl;//2
     
-    cout << angka[0][0][0] << endl;//1
-    cout << angka[0][0][1] << endl;//2
+    cout << angka[0][1][0] << endl;//3
+    cout << angka[0][1][1] << endl;//4
     
-    cout << angka[0][0][0] << endl;//1
-    cout << angka[0][0][1] << endl;//2
+    cout << angka[1][0][0] << endl;//5
+    cout << angka[1][0][1] << endl;//6
     
+    cout << angka[1][1][0] << endl;//7
+    cout << angka[1][1][1] << endl;//8
+
+    system("pause");
+    return 0;    
     
 }
