92ee52aaed0f775d865fa5e61d31bfc1268557f9
Asep <88421251+Cipaxdragon@users.noreply.github.com>
2021-11-06 19:23:16 +0800

    adding files asistensi 2

---
+++ Tugas_Prak_4/Asistensi_2.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+	int x = 0;
+	string nama[5];
+	string nim[5];
+	int nilai[5];
+
+	//inputan 1
+	cout << "\n";
+	cout << "=== 1 ===" << endl;
+
+	cout << "Masukan Nama => ";
+	cin >> nama[x];
+
+	cout << "Masukan Nim => ";
+	cin >> nim[x];
+
+	cout << "Masukan nilai akhir => ";
+	cin >> nilai[x];
+
+	//inputan 2
+	cout << "\n";
+	 cout << "=== 2 ===" << endl;
+
+	x++;
+	cout << "Masukan Nama => ";
+	cin >> nama[x];
+
+	cout << "Masukan Nim => ";
+	cin >> nim[x];
+
+	cout << "Masukan nilai akhir => ";
+	cin >> nilai[x];
+	cout << "\n";
+
+	//inputan 3
+	 cout << "\n";
+	 cout << "=== 3 ===" << endl;
+
+	 x++;
+	 cout << "Masukan Nama => ";
+	 cin >> nama[x];
+
+	 cout << "Masukan Nim => ";
+	 cin >> nim[x];
+
+	 cout << "Masukan nilai akhir => ";
+	 cin >> nilai[x];
+	 cout << "\n";
+
+	//inputan 4
+	 cout << "\n";
+	 cout << "=== 4 ===" << endl;
+	 x++;
+	 cout << "Masukan Nama => ";
+	 cin >> nama[x];
+
+	 cout << "Masukan Nim => ";
+	 cin >> nim[x];
+
+	 cout << "Masukan nilai akhir => ";
+	 cin >> nilai[x];
+	 cout << "\n";
+
+
+//Data Tertinggi
+//0
+	x = 0;
+	if(nilai[x] > nilai[1] and nilai[x] > nilai[2] and nilai[x] > nilai[3] and nilai[x] > nilai[4]){
+	cout << "Nilai Tertinggi : " << endl;
+	cout << "Nama :" << nama[0] << endl;
+	cout << "NIM  :" << nim[0] << endl;
+	cout << "Nilai  :" << nilai[0] << endl;
+	x++;
+	}
+//1
+
+	else if (nilai[1] > nilai[0] and nilai[1] > nilai[2] and nilai[1] > nilai[3] and nilai[1] > nilai[4]){
+	cout << "Nilai Tertinggi : " << endl;
+	cout << "Nama :" << nama[1] << endl;
+	cout << "NIM  :" << nim[1] << endl;
+	cout << "Nilai  :" << nilai[1] << endl;
+	x++;
+	}
+
+//2
+
+	else if (nilai[2] > nilai[0] and nilai[2] > nilai[1] and nilai[2] > nilai[3] and nilai[2] > nilai[4]){
+	cout << "Nilai Tertinggi : " << endl;
+	cout << "Nama :" << nama[2] << endl;
+	cout << "NIM  :" << nim[2] << endl;
+	cout << "Nilai  :" << nilai[2] << endl;
+	x++;
+	}
+
+//3
+	
+	else if (nilai[3] > nilai[1] and nilai[3] > nilai[2] and nilai[3] > nilai[0] and nilai[3] > nilai[4]){
+	cout << "Nilai Tertinggi : " << endl;
+	cout << "Nama :" << nama[3] << endl;
+	cout << "NIM  :" << nim[3] << endl;
+	cout << "Nilai  :" << nilai[3] << endl;
+	x++;
+	}
+
+
+	
+	cout <<  endl;
+
+
+    int ratarata;
+    ratarata = (nilai[0] + nilai[1] + nilai[2] + nilai[3]) : 4;
+
+    cout << "Rata Rata 4 Inputan = " << ratarata << endl;
+
+	system("pause");		
+}
