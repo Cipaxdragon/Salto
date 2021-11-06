e5de48b68d601e2f6b34964ecfd394ce3e39b6ef
Asep <88421251+Cipaxdragon@users.noreply.github.com>
2021-11-06 08:31:38 +0800

    revisi_1

--- Tugas_Prak_4/Array Kelas A.cpp
+++ Tugas_Prak_4/Array Kelas A.cpp
@@ -4,64 +4,122 @@ using namespace std;
 
 int main(){
 
-//Variabel
-	string nama[2];
-	char bah[2]; // bahasa pemrograman
-	int nilai[2]; // 0 -> 100
-	string noreg[2];
+	int x = 0;// variabel array
 
-//Judul
-cout << "== Program Mencari Nilai maksimum == \n\n" << endl;
-
-//Data Masukan 
-
-// #1
-	cout << "=== Data Peserta 1 ===" << endl;
-
-	cout << "No Registrasi\t:";
-	cin >> noreg[0];
+	//Variabel data
+	int noreg[6];
+	string nama[6];
+	char bahasa[6];
+	int nilai[6];
+	cout << "=== Data ===" << endl;
 
 
-	//JIka inputan tidak 6 digit
-		int i = noreg[0].size();
-		if(!(i <= 6)){
-			cout << "Inputan Max 6 digit" << endl;
-			system ("pause");
-			return 0;
-		}
+//Orang Pertama
+	cout << "=== Peserta Pertama === " << endl;
 
-	cout << "Nama Peserta\t:";
-	cin >> nama[0];
+	cout << "No registrasi\t => ";
+	cin >> noreg[x];
 
-	cout << "Bahasa Pemrograman\t:";
-	cin >> bah;
-	// bah = toupper(bah[0]);
+	cout << "Nama Peserta\t => ";
+	cin >> nama[x];
 
-	cout << "Nilai Ujian \t:"; //0 - 100
-	cin >> nilai[0];
+	cout << "Bahasa Pemrograman\t => ";
+	cin >> bahasa [x];
+
+	cout << "Nilai Ujian\t => ";
+	cin >> nilai[x];
+
+	
+//Orang Kedua
+	x++; // x = 1
+	cout << "=== Peserta Kedua === " << endl;
+
+	cout << "No registrasi\t => ";
+	cin >> noreg[x];
+
+	cout << "Nama Peserta\t => ";
+	cin >> nama[x];
+
+	cout << "Bahasa Pemrograman\t => ";
+	cin >> bahasa [x];
+
+	cout << "Nilai Ujian\t => ";
+	cin >> nilai[x];
 
 
-//Seleksi Kelas
-	string kelas1;
-	if(bah[0] == 'B'){
-		kelas1 = "Basic";
-	}
-	else if (bah[0] == 'P'){
-		kelas1 = "Pascal";
-	}
-	else if (bah[0] == 'C'){
-		kelas1 = "C++";
-	}
+	cout << endl;
+	cout << nama[0] << nama[1];
+
+//Orang 3
+	x++; // x = 2
+	cout << "=== Peserta Ketiga === " << endl;
+
+	cout << "No registrasi\t => ";
+	cin >> noreg[x];
+
+	cout << "Nama Peserta\t => ";
+	cin >> nama[x];
+
+	cout << "Bahasa Pemrograman\t => ";
+	cin >> bahasa [x];
+
+	cout << "Nilai Ujian\t => ";
+	cin >> nilai[x];
+
+//Orang 4
+	x++; // x = 3
+	cout << "=== Peserta Keempat === " << endl;
+
+	cout << "No registrasi\t => ";
+	cin >> noreg[x];
+
+	cout << "Nama Peserta\t => ";
+	cin >> nama[x];
+
+	cout << "Bahasa Pemrograman\t => ";
+	cin >> bahasa [x];
+
+	cout << "Nilai Ujian\t => ";
+	cin >> nilai[x];
+
+//Orang 5
+	x++; // x = 4
+	cout << "=== Peserta Kelima === " << endl;
+
+	cout << "No registrasi\t => ";
+	cin >> noreg[x];
+
+	cout << "Nama Peserta\t => ";
+	cin >> nama[x];
+
+	cout << "Bahasa Pemrograman\t => ";
+	cin >> bahasa [x];
+
+	cout << "Nilai Ujian\t => ";
+	cin >> nilai[x];
+
+//Orang 6
+	x++; // x = 5
+	cout << "=== Peserta Keenam === " << endl;
+
+	cout << "No registrasi\t => ";
+	cin >> noreg[x];
+
+	cout << "Nama Peserta\t => ";
+	cin >> nama[x];
+
+	cout << "Bahasa Pemrograman\t => ";
+	cin >> bahasa [x];
+
+	cout << "Nilai Ujian\t => ";
+	cin >> nilai[x];
+
+
+//Proses
+
 
 //Data Keluaran
+	cout << "=== Data Keluaran ===" << endl;
 
-	cout << "=== Data Peserta ===" << endl;
-
-	cout << "#Kelas Bahasa " << kelas1 << endl;
-	cout << "Nama Peserta = " << nama[0] << endl; 
-	cout << "Nomor Registrasi = " << noreg [0] << endl; 
-	cout << "Nilai Ujiann = " << nilai[0] << endl;
-	
-//Program Nilai Maksimal 
 	system("pause");
 }
