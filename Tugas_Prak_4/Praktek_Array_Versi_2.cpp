d4c756c5073952a904584d7cd88d760f98a1627c
Asep <88421251+Cipaxdragon@users.noreply.github.com>
2021-11-06 18:03:23 +0800

    066

--- Tugas_Prak_4/Praktek_Array_Versi_2.cpp
+++ Tugas_Prak_4/Praktek_Array_Versi_2.cpp
@@ -4,106 +4,104 @@ using namespace std;
 
 int main(){
 	
-	int x = 0;
-	int digit;
-	char kelas;
-	string nama[2];
+	int x_51 = 0;
+	int digit_51;
+	char kelas_51;
+	string nama_51[2];
 	string noreg[2];
 	string judul,akhir;
-	int nilai[2];
-	cout << "Pilih Kelas Bahasa Pemrograman" << endl;
+	int nilai_51[2];
+
+	cout << "Pilih kelas Bahasa Pemrograman" << endl;
 	cout << "1. Basic (B)" << endl;
 	cout << "2. Pascal (P)" << endl;
 	cout << "3. C++ (C)" << endl;
 
 
 	cout << "[B/P/C]====> ";
-	cin >> kelas;
-	kelas = toupper(kelas);
-	if(kelas == 'B'){
-		judul = "=== Kelas Bahasa Basic ===";
-		akhir = "Nilai Ujian Tertinggi Di kelas basic = > ";
+	cin >> kelas_51;
+
+	if(kelas_51 == 'B'){
+		judul = "=== kelas Bahasa Basic ===";
+		akhir = "nilai Ujian Tertinggi Di kelas basic = > ";
 
 
 	}
-	else	if(kelas == 'P'){
-		judul = "=== Kelas Bahasa Pascal ===";
-		akhir = "Nilai Ujian Tertinggi Di kelas basic = > ";
+	else	if(kelas_51 == 'P'){
+		judul = "=== kelas Bahasa Pascal ===";
+		akhir = "nilai Ujian Tertinggi Di kelas basic = > ";
 	}
-	else	if(kelas == 'C'){
-		judul = "=== Kelas Bahasa C++ ===";
-		akhir = "Nilai Ujian Tertinggi Di kelas basic = > ";
+	else	if(kelas_51 == 'C'){
+		judul = "=== kelas Bahasa C++ ===";
+		akhir = "nilai Ujian Tertinggi Di kelas basic = > ";
 
 	}
 
 	cout << judul << endl;
 	cout << "\n\n";
 	cout << "=== Peserta 1 ===" << endl;
-	cout << "Nama peserta => ";
-	cin >> nama[x];
+	cout << "nama peserta => ";
+	cin >> nama_51[x_51];
 
 	cout << "No Registrasi => ";
-	//Jika Tidak 6 Digit
-	cin >> noreg[x];
-	digit = noreg[x].size();
 
-		if(!(digit <= 6)){
+	cin >> noreg[x_51];
+	digit_51 = noreg[x_51].size();
+
+		if(!(digit_51 <= 6)){
 			cout << "Maximal Nomor Registrasi Cuma 6 digit" << endl;
-			system("pause"); main();
+			return 0;
 		}
 
-	cout << "Nilai Ujian => ";
-	cin >> nilai[x];
-		if(!(nilai[x] >= 0 and nilai[x] <= 100)){
-		cout << "Nilai cuma 0 - 100" << endl;
-			system("pause"); main();	
+	cout << "nilai Ujian => ";
+	cin >> nilai_51[x_51];
+		if(!(nilai_51[x_51] >= 0 and nilai_51[x_51] <= 100)){
+		cout << "nilai cuma 0 - 100" << endl;
+			return 0;	
 		}
 	cout << "\n\n";
-	x++;
+	x_51++;
 	cout << "=== Peserta 2 ===" << endl;
-	cout << "Nama peserta => ";
-	cin >> nama[x];
+	cout << "nama peserta => ";
+	cin >> nama_51[x_51];
 
 	cout << "No Registrasi => ";
-	cin >> noreg[x];
-	//Jika Tidak 6 Digit
-	digit = noreg[x].size();
-	if(!(digit <= 6))
+	cin >> noreg[x_51];
+
+	digit_51 = noreg[x_51].size();
+	if(!(digit_51 <= 6))
 		{
 			cout << "Maximal Nomor Registrasi Cuma 6 digit" << endl;
-			system("pause"); main();
+			return 0;
 		}
 
-	cout << "Nilai Ujian => ";
-	cin >> nilai[x];
-		if(!(nilai[x] >= 0 and nilai[x] <= 100)){
-		cout << "Nilai cuma 0 - 100" << endl;
-			system("pause"); main();	
+	cout << "nilai Ujian => ";
+	cin >> nilai_51[x_51];
+		if(!(nilai_51[x_51] >= 0 and nilai_51[x_51] <= 100)){
+		cout << "nilai cuma 0 - 100" << endl;
+			return 0;	
 		}
 
-	x = 0;
+	x_51 = 0;
 	cout << "\n\n";
 	cout << "=== Hasil Data ====" << endl;
 	cout << "=== Peserta Pertama ===" << endl;
-	cout << "Nama Peserta \t: " << nama[x] << endl;
-	cout << "No Registrasi \t: " << noreg[x] << endl;
-	cout << "Nilai Ujian  \t: " << nilai[x] << endl;
+	cout << "nama Peserta \t: " << nama_51[x_51] << endl;
+	cout << "No Registrasi \t: " << noreg[x_51] << endl;
+	cout << "nilai_51 Ujian  \t: " << nilai_51[x_51] << endl;
 	cout << "\n\n";
-	x++;
+	x_51++;
 	cout << "=== Peserta Kedua ===" << endl;
-	cout << "Nama Peserta \t: " << nama[x] << endl;
-	cout << "No Registrasi \t: " << noreg[x] << endl;
-	cout << "Nilai Ujian  \t: " << nilai[x] << endl;
+	cout << "nama Peserta \t: " << nama_51[x_51] << endl;
+	cout << "No Registrasi \t: " << noreg[x_51] << endl;
+	cout << "nilai Ujian  \t: " << nilai_51[x_51] << endl;
 	cout << "\n\n";
 
-	if (nilai[0] > nilai[1]){
-		cout << akhir << nama[0] << endl;
+	if (nilai_51[0] > nilai_51[1]){
+		cout << akhir << nama_51[0] << endl;
 	}
 	else {
-		cout << akhir << nama[1] << endl;
+		cout << akhir << nama_51[1] << endl;
 	}
-
-	system("pause");
-	main();
 
 }
