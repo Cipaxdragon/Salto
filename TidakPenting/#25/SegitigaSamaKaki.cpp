2ed3bc237299817a8e962baaea1f8fe0ee5d7b77
Asep <88421251+aseptod@users.noreply.github.com>
2021-11-13 11:11:38 +0800

    awoka

--- TidakPenting/#25/SegitigaSamaKaki.cpp
+++ TidakPenting/#25/SegitigaSamaKaki.cpp
@@ -3,23 +3,21 @@
 using namespace std;
 
 int main(){
-    int n;
-    cout << "Masukan Tinggi Segitiga => ";
-    cin >> n;
-    //Segitiga  Siku Siku 1
-    printf("Segitiga Siku Siku 1\n\n");
+	// main Loop
+	for(int i = 1; i <= 10;i++){
+		
+		//Pola Satu
+		for(int j = 10; j >= i;j--){
+			cout << " ";
+		}
 
-    //Main Loop
-    for(int i = 1;i <= n;i++){
-        //Looping Pola Segitiga
-        for(int j = n; j >= i;j--){
-            cout << " ";
-        }//Spasi Sisi Kiri
+		//Pola 2
+		for(int k = )
 
 
-        for(int k = 1;k<) 
-        cout << endl;
-    }
+
+		cout << endl;
+	}
 
 
 }
---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	string nama[3],NIK[3];
+	int djam[3,dmenit[3];
+	int pjam[3],pmenit;
+	int x;
+	string nilai;
+	for(int i = 1; i <= )
+	x = 0;
+	cout << "Masukan Nama = ";
+	cin >> nama;
+	cout << "Masukan NIK = ";
+	cin >> NIK;
+
+	cout << "Jam Datang = ";
+	cin >> djam;
+
+	cout << "Menit Datang = ";
+	cin >> dmenit;
+
+	cout << "Jam Pulang = ";
+	cin >> djam;
+
+	cout << "Menit Pulang = ";
+	cin >> dmenit;
+
+
+	if(djam < 8 ){
+		cout
+	}
+	
+
+
+	
+} 
