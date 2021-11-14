b17235064bacecec89158cc5b630f168f3d637c7
Asep <88421251+Cipaxdragon@users.noreply.github.com>
2021-11-14 16:26:15 +0800

    awokaowkoqa

--- TidakPenting/#25/SegitigaSamaKaki.cpp
+++ TidakPenting/#25/SegitigaSamaKaki.cpp
@@ -3,21 +3,38 @@
 using namespace std;
 
 int main(){
-	// main Loop
-	for(int i = 1; i <= 10;i++){
-		
-		//Pola Satu
-		for(int j = 10; j >= i;j--){
-			cout << " ";
-		}
 
-		//Pola 2
-		for(int k = )
+//Segitiga Sama Kaki
 
+    //Segitiga Tidak Terbalik
+    printf("Pola Segitiga 1\n");
 
+    for(int i = 1; i <= 10;i++){
+        
+        //Pola Satu
+        for(int j = 10; j > i;j--){
+            cout << " ";
+        }
+        for(int k = 1; k <= (2 * i - 1);k++){
+            cout << "*";
+        }
+        cout << endl;
 
-		cout << endl;
-	}
+    }
+//Segitiga Terbalik
+    cout << endl;
+    printf("Pola Segitiga 2\n");
 
+    //Segitga Terbalik
+    for(int i = 1; i <= 10;i++){
+        for(int k = 1; k < i; k++){
+            cout << " ";
+        }
+        for (int j = 10; j >= (2 * i - 10);j--){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    system("pause");
 
 }
