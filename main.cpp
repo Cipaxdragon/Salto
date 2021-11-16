e093f4664a4d4374c2fb0f9cc1edd6f6a2340696
Asep <88421251+Cipaxdragon@users.noreply.github.com>
2021-11-16 19:08:53 +0800

    anjayasdasd

--- main.cpp
+++ main.cpp
@@ -3,54 +3,30 @@
 using namespace std;
 
 int main(){
-	string nama[3],NIK[3];
-	int djam[3],dmenit[3];
-	int pjam[3],pmenit[3];
-	int x;
-	string rm[3];
-	for(int i = 1; i <= 3;i++){
-	x = 0;
-    cout << "Peserta " << i << endl;
-	cout << "Masukan Nama = ";
-	cin >> nama[x];
-	cout << "Masukan NIK = ";
-	cin >> NIK[x];
 
-	cout << "Jam Datang = ";
-	cin >> djam[x];
-
-	cout << "Menit Datang = ";
-	cin >> dmenit[x];
-
-	cout << "Jam Pulang = ";
-	cin >> djam[x];
-
-	cout << "Menit Pulang = ";
-	cin >> dmenit[x];
-
-
-	if(djam[x] == 8 and pjam[x] == 16 and ){
-        rm[x] = "Rajin";
+	int y;
+	int j;
+	cout << "Masukan Jumlah Data : "; cin >> j;
+	string nama[j],nim[j],kelas[j];
+	for(int i = 0;i < j;i++){
+		y = i + 1;
+		cout << "Peserta " << y << endl;
+		cout << "Masukan Nama "<< y <<" : "; cin.ignore();getline(cin,nama[i]);
+		cout << "Masukan NIM "<< y <<"  : "; cin.ignore();getline(cin,nim[i]);
+		cout << "Masukan Kelas "<< y << ": ";cin.ignore();getline(cin,kelas[i]);
 	}
-    else if (djam[x] < 8 and pjam[x] == 16){
-        rm[x] = "Sangat Rajin";
-    }
-     else if (djam[x] > 8 and pjam[x]  == 16){
-        rm[x] = "Malas";
-    }
-    else if (djam[x] > 8 and pjam[x] < 16){
-        rm[x] = "Sangat Malas";
-    }
-
-    cout << "Hasil Data Keluaran" << endl;
-
-    cout << "Nama :"<< nama[x] << endl;
-    cout << "NIK  :"<< NIK[x] << endl;
-    cout << "Dia adalah Pegawai  : "<< rm[x] << endl;
-
-
-    x++;
 	
-    } 
+	cout << "==================" << endl;
+	y = 0;
+	for(int i = 0;i < j;i++){
+		y = i + 1;
+		cout << "Data Keluaran Peserta " << y << endl;
+		cout << "Nama  "<< y <<"  : " << nama[i] << endl;
+		cout << "NIM   "<< y <<"  : " << nim[i] << endl;
+		cout << "Kelas "<< y <<"  : " << kelas[i] << endl;
 
-} 
+	}
+	cout << "==================" << endl;
+	
+		
+}
