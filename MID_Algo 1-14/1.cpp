99426a434f6544197771c42ebb3e7f8de86f0ac0
Asep <88421251+Cipaxdragon@users.noreply.github.com>
2022-01-02 20:15:14 +0800

    Update 1.cpp

--- MID_Algo 1-14/1.cpp
+++ MID_Algo 1-14/1.cpp
@@ -7,7 +7,7 @@ int main(){
 	cout << "=== Bilangan Ganjil Kelipatan 31 ===" << endl;
 	cout << endl;
 	for(int i = 20+1; i<4903; i++){
-		if(i % 2 == 1 and i % 31){
+		if(i % 2 == 1 and i % 31 == 0){
 			cout << i << endl;
 		}
 	}
